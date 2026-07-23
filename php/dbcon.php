135db97dccd83962df49b8bf9bfe21954824e760
honeyehsayyadi13-hub <honeyehsayyadi13@gmail.com>
2026-07-23 13:38:34 -0400

    updated

--- php/dbcon.php
+++ php/dbcon.php
@@ -1,13 +1,13 @@
 <?php
-//database connection settings for mywen server
+//database connection settings for myweb server
 $host="localhost";
 $dbname="sayyadi_company_db";
 $username="sayyadi_company_db";
 $password="PbPBKsu4xtyZtW6cVTsw";
 
 try{//establishing PDO connection
-    $pdo= new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4",$username,$password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn= new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4",$username,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 }catch (PDOException $e){//if the connection failed then a message will appear
     die("Database connection failed: " . $e->getMessage());
